b4e75dc94e014b154d7b19c0901d77c42a0f4ec4
Carlos Bauluz <c.bauluz.04@gmail.com>
2025-04-25 17:15:26 +0200

    DONE

--- islands/PersonajesC.tsx
+++ islands/PersonajesC.tsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from "preact/hooks";
 import { FunctionalComponent } from "preact/src/index.d.ts";
 
-const Personajes: FunctionalComponent = () => { 
+const Personajes: FunctionalComponent = () => {
   const [name, setName] = useState<string[]>([("")])
-  //const [name2, setName2] = useState<string>("")
+  const [name2, setName2] = useState<string>("")
   const [error, setError] = useState<boolean>(false);
-  const [num = 1, setNum] = useState<number>()
-  const getCharacters = async (num: string)=> { 
+  const [num, setNum] = useState<number>(1)
+  const getCharacters = async (num: number) => {
     
     const response = await fetch(`https://rickandmortyapi.com/api/character?page=${num}`)
     const data = await response.json()
-    const nombres:string[] = data.results.map((o) => o.name)
+    const nombres: string[] = data.results.map((o) => o.name)
     
     setName(nombres);
   }
@@ -19,7 +19,7 @@ const Personajes: FunctionalComponent = () => {
     if (!num || num >= 41) {
       setError(true)
       setNum(num)
-    } else { 
+    } else {
       const a = num + 1
       setNum(a)
     }
@@ -30,19 +30,31 @@ const Personajes: FunctionalComponent = () => {
     if (!num || num <= 1) {
       setError(true)
       setNum(num)
-    } else { 
-      const a = num -1
+    } else {
+      const a = num - 1
       setNum(a)
     }
   }
 
-  useEffect(() => getCharacters(num), [num] )
+  useEffect(() => getCharacters(num), [num])
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      getCharacters(num);
+    }, 1000); 
+
+    return () => clearTimeout(timer); 
+  }, [num]);
+
   return (
     <div>
       {setError(false)}
       <button type="Next" onClick={handleClick1}>Next</button>  
       <button type="Back" onClick={handleClick2}>Back</button>
       {error && <div>El limite es la página {num}</div>}
+      <input type="text" name="nombre" placeholder="Nombre" value={name2} onInput={
+        setError(false)
+        
+        } />
       <h1>Ricky List</h1>
       <ul>
           {name.map((e) => 
